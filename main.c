30b09b06ac4fc26f5379d9204ca69fc4ac8ccc21
JiriHofirek <jirixskola@gmail.com>
2021-04-17 16:32:17 +0200

    .gitignore file added
    main.c:
        procesing args pased to function
        a bug fixed - flags started with -s are no longer consderd to be eual -s
            (similarly with the second flag -h)

--- main.c
+++ main.c
@@ -7,71 +7,16 @@
 #define DELAULT_BOARD_SIZE 15
 #define HELLP "Console Gomoku Game\n" "FLAGS:\n" " -s     --size          Size of game board.\n" " -h     --hellp         Prints hellp menu.\n"
 
+#define OK_CONTINUE 128
 
+/*
+ * processes the args
+ * !! in some cases can call exit and end the program !!
+ */ 
+int process_args(int argc, char* argv[]);
 
 int main(int argc, char* argv[]) {
-    int size = DELAULT_BOARD_SIZE;
-
-    /* process args */
-    for (int i=1; i<argc; ++i) {
-        // valid flag starts with '-' and continues with a char
-        if (argv[i][0] != '-' || argv[i][1] == '\0') {
-            fprintf(stderr, "error: argument %i expected to be flag\n", i);
-            return EXIT_FAILURE;
-        }
-
-        if (argv[i][1] == 's') {
-            // counter incremented in side clycle
-            // next arg is not expected to be a flag so ti doesn't have to be tested as a flag
-            ++i;
-            if (i >= argc) {
-                fprintf(stderr,
-                        "error: argument %i is size flag, argument %i is expected\n", i-1, i);
-                return EXIT_FAILURE;
-            }
-
-            size = atoi(argv[i]);
-            if (size < WINNING) {
-                fprintf(stderr,
-                        "error: the size of the board (%i) is lower then winning number of stones in the row (%i)\n", size, WINNING);
-                return EXIT_FAILURE;
-            }
-            continue;
-        }
-        if (argv[i][1] == 'h') {
-            printf(HELLP);
-            // if hellp flag passed no other flags matter
-            return EXIT_SUCCESS;
-
-        }
-        if (argv[i][1] == '-') {
-            if (strcmp(argv[i], "--size") == 0) {
-                // counter incremented in side clycle
-                // next arg is not expected to be a flag so ti doesn't have to be tested as a flag
-                ++i;
-                if (i >= argc) {
-                    fprintf(stderr,
-                            "error: argument %i is size flag, argument %i is expected\n", i-1, i);
-                    return EXIT_FAILURE;
-                }
-
-                size = atoi(argv[i]);
-                if (size < WINNING) {
-                    fprintf(stderr,
-                            "error: the size of the board (%i) is lower then winning number of stones in the row (%i)\n", size, WINNING);
-                    return EXIT_FAILURE;
-                }
-                continue;
-            }
-            if (strcmp(argv[i], "--hellp") == 0) {
-            printf(HELLP);
-            // if hellp flag passed no other flags matter
-            return EXIT_SUCCESS;
-            }
-        } // end if
-        fprintf(stderr, "error: argument %i is unknowen flag\n", i);
-        return EXIT_FAILURE;
-    } // end for
+    int size = process_args(argc, argv);
 
     int *board = alloc_board(size);
     if (board == NULL) {
@@ -95,4 +40,76 @@ int main(int argc, char* argv[]) {
 
     free(board);
     return EXIT_SUCCESS;
+}
+
+
+/*
+ * processes the args
+ * !! in some cases can call exit and end the program !!
+ */
+int process_args(int argc, char* argv[]) {
+    int size = DELAULT_BOARD_SIZE;
+
+    /* process args */
+    for (int i=1; i<argc; ++i) {
+        // valid flag starts with '-' and continues with a char
+        if (argv[i][0] != '-' || argv[i][1] == '\0') {
+            fprintf(stderr, "error: argument %i expected to be flag\n", i);
+            exit(EXIT_FAILURE);
+        }
+
+        if (strcmp(argv[i], "-s") == 0) {
+            // counter incremented in side clycle
+            // next arg is not expected to be a flag so ti doesn't have to be tested as a flag
+            ++i;
+            if (i >= argc) {
+                fprintf(stderr,
+                        "error: argument %i is size flag, argument %i is expected\n", i-1, i);
+                exit(EXIT_FAILURE);
+            }
+
+            size = atoi(argv[i]);
+            if (size < WINNING) {
+                fprintf(stderr,
+                        "error: the size of the board (%i) is lower then winning number of stones in the row (%i)\n", size, WINNING);
+                exit(EXIT_FAILURE);
+            }
+            continue;
+        }
+        if (strcmp(argv[i], "-h") == 0) {
+            printf(HELLP);
+            // if hellp flag passed no other flags matter
+            exit(EXIT_SUCCESS);
+
+        }
+        if (argv[i][1] == '-') {
+            if (strcmp(argv[i], "--size") == 0) {
+                // counter incremented in side clycle
+                // next arg is not expected to be a flag so ti doesn't have to be tested as a flag
+                ++i;
+                if (i >= argc) {
+                    fprintf(stderr,
+                            "error: argument %i is size flag, argument %i is expected\n", i-1, i);
+                    exit(EXIT_FAILURE);
+                }
+
+                size = atoi(argv[i]);
+                if (size < WINNING) {
+                    fprintf(stderr,
+                            "error: the size of the board (%i) is lower then winning number of stones in the row (%i)\n", size, WINNING);
+                    exit(EXIT_FAILURE);
+                }
+                continue;
+            }
+            if (strcmp(argv[i], "--hellp") == 0) {
+            printf(HELLP);
+            // if hellp flag passed no other flags matter
+            exit(EXIT_SUCCESS);
+            }
+        } // end if
+        fprintf(stderr, "error: argument %i is unknowen flag\n", i);
+        exit(EXIT_FAILURE);
+    } // end for
+
+    return size;
 }
